fc68572eb0b89b15afd9ff87b3f3829f0fe66248
VojtechNovak <vojta.novak2005@gmail.com>
2026-07-14 15:16:48 +0200

    feat: easier connection to database for deployment

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,19 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-});
+const pool = new Pool(
+  process.env.POSTGRES_URL
+    ? {
+        connectionString: process.env.POSTGRES_URL,
+        // Vercel vyžaduje SSL
+        ssl: { rejectUnauthorized: false }
+      }
+    : {
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_DB || process.env.POSTGRES_DATABASE,
+        password: process.env.POSTGRES_PASSWORD,
+        port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+      }
+);
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
